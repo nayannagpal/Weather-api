02d125b06a7b6dd587af1aeaa59cfe29b1f10aab
Nayan Nagpal <110152854+nayannagpal@users.noreply.github.com>
2022-11-09 19:28:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = 'd284596b90a571d54401a1891ef0bff5'
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No city found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}°F")
